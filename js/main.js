c0c98c042f11346534cf0115972079a0763af701
George <gosha3484@gmail.com>
2025-04-08 20:51:09 +0300

    added also profile to about page

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 import { addMarker, clearMarkers } from './map.js';
 import { fetchRestaurants, fetchRestaurantMenu } from './api.js';
 import { updateFavoriteRestaurant } from './favorite.js';
+import { loadUserProfile } from './profileDisplay.js';
 
 let selectedRestaurant = null;
 let allRestaurants = [];
@@ -22,33 +23,7 @@ const menuTypeBtns = document.querySelectorAll('.menu-type-btn');
 // Initialize the application
 document.addEventListener('DOMContentLoaded', async () => {
 
-    const token = localStorage.getItem('token');
-    if (token) {
-        try {
-            const res = await fetch('https://media2.edu.metropolia.fi/restaurant/api/v1/users/token', {
-                headers: {
-                    'Authorization': `Bearer ${token}`
-                }
-            });
-
-            const data = await res.json();
-
-            if (res.ok && data.username) {
-                const userNameEl = document.getElementById('user-name');
-                const userAvatarEl = document.getElementById('user-avatar');
-                const userInfoBox = document.getElementById('user-info');
-
-                userNameEl.textContent = data.username;
-                userInfoBox.style.display = 'flex';
-
-                if (data.avatar) {
-                    userAvatarEl.src = `https://media2.edu.metropolia.fi/restaurant/uploads/${data.avatar}`;
-                }
-            }
-        } catch (error) {
-            console.error('Käyttäjätiedot epäonnistui:', error);
-        }
-    }
+    loadUserProfile();
 
     try {
         allRestaurants = await fetchRestaurants();
